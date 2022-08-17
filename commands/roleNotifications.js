315fc29bdeffb9338056327b9c5da121e4e7de93
Wesley Bijleveld <6079485+Aimless321@users.noreply.github.com>
2022-08-17 20:15:13 +0200

    feat: add role-notifications

---
+++ commands/roleNotifications.js
@@ -0,0 +1,53 @@
+const {SlashCommandBuilder} = require("discord.js");
+const {RoleNotification} = require("../models");
+
+function validateParameters(interaction, role, content) {
+    if (!role) {
+        interaction.reply({content: 'Invalid role selected', ephemeral: true});
+        return false;
+    }
+
+    if (!content) {
+        interaction.reply({content: 'Please specify a message', ephemeral: true});
+        return false;
+    }
+
+    try {
+        JSON.parse(content)
+    } catch (error) {
+        interaction.reply({content: 'Invalid message JSON', ephemeral: true})
+        return false;
+    }
+
+    return true;
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('role-notification')
+        .setDescription('Send users a DM when they receive a role')
+        .addRoleOption(option => option.setName('role').setDescription('Role the notification has to work for'))
+        .addStringOption(option => option.setName('message').setDescription('Content of the DM')),
+    async execute(interaction) {
+        const role = interaction.options.getRole('role');
+        const content = interaction.options.getString('message');
+
+        if (!validateParameters(interaction, role, content)) {
+            return;
+        }
+
+        let notificationModel = await RoleNotification.findOne({where: {role: role.id}});
+
+        if (notificationModel) {
+            notificationModel.message = content;
+            await notificationModel.save();
+
+            return interaction.reply({content: 'Updated role notification', ephemeral: true});
+        }
+
+        notificationModel = RoleNotification.build({role: role.id, message: content});
+        await notificationModel.save();
+
+        return interaction.reply({content: 'Saved role notification', ephemeral: true});
+    },
+};
---
+++ models/rolenotification.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class RoleNotification extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  RoleNotification.init({
+    role: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    message: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'RoleNotification',
+  });
+  return RoleNotification;
+};
