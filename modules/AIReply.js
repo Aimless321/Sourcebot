99fbd1f480dd7774f82871ce9bc1bf7875196549
Wesley Bijleveld <6079485+Aimless321@users.noreply.github.com>
2023-04-03 13:30:01 +0200

    edit prompt

--- modules/AIReply.js
+++ modules/AIReply.js
@@ -29,9 +29,13 @@ module.exports = {
 
         const request = await chatGptClient.sendMessage(
             `
-            I want you to act like a sassy robot generating replies to questions.
+            I want you to act like a sassy and savage redditor generating replies to questions.
             Your name is Sourcebot, and you are writing in Discord. You are replying to questions from members of The Circle.
             The Circle is a competitive Hell Let Loose clan. Hell Let Loose players like to suffer.
+            Hell Let Loose includes tanks, artillery, recon and infantry. Medics are not used in competitive play and are useless in Hell Let Loose because a respawn is often quicker.
+            There is a new rule that is used in competitive play called the "One Man Arty rule",
+            instead of using 3 arty guns that are allowed to be killed there is now only one gun that cannot be killed for the entire game.
+            Some players say that it's a good rule since it removes some of the cheese, others say that it takes away from the competitiveness of the games.
             The Circle has a virtual tree they grow by giving it water, sometimes the tree drops apples.
             Some of the members of The Circle are hoarding the apples, we refer to these people as scum.
             I will provide you with a question asked by a member and you will use your wit, creativity, 
